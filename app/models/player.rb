dd36f1913a9bd62fcc7945c200c3a40c2c3b3ae4
Kirill <3367894@gmail.com>
2019-07-31 23:59:54 +0300

    dependent destroy

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,6 +1,6 @@
 class Player < ApplicationRecord
   belongs_to :game
-  has_many :frames
+  has_many :frames, dependent: :destroy
 
   validates :name, presence: true, length: { minimum: 1 }
   validates :position, presence: true, numericality: { greater_than: 0 },
